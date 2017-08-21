545aac464cccb4354d0b54dcec1d4f9a58d4d74e
bylylaka <maxim_arslanov@mail.ru>
2017-08-21 23:19:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,11 @@ function prov_fio() {
 function prov_phone() {
     var adr=document.getElementById("phone").value;
     if(adr) {
-        var sum = parseInt(adr.charAt(1))+parseInt(adr.charAt(3))+parseInt(adr.charAt(4))+parseInt(adr.charAt(5))+parseInt(adr.charAt(7))+parseInt(adr.charAt(8))+parseInt(adr.charAt(9))+parseInt(adr.charAt(11))+parseInt(adr.charAt(12))+parseInt(adr.charAt(14))+parseInt(adr.charAt(5));
+        sum=0;
+        for (var i = 0; i < 16; i++) {
+            if (!isNaN(phone.value.charAt(i)))
+                sum += parseInt(phone.value.charAt(i));
+        }
         if (sum<=30)
             return true;
     }
